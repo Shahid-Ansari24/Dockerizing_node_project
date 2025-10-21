70e96741668faa260bdafa35adfec0698023d2d7
Shahid Ansari <shahidansari2452@gmail.com>
2025-10-21 16:24:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send({message: "Hello World!"});
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("server started at port 3000");
 })
